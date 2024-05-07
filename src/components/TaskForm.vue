e9ab5017687c3da3b095abb0be80832ec20c8c4a
DjMike <michaelchang277@gmail.com>
2024-05-06 20:42:01 -0600

    separeción de backend

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -108,5 +108,4 @@
         <input type="text" id="job" v-model="updatedUser.job" />
         <button type="submit">Submit</button>
     </form>
-    <img src="../assets/vue.svg" alt="" draggable="true" />
 </template>
